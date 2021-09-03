ac7aa87801f88592fbe0c35a64f23366efafc68c
c0venn <fabichidori2@gmail.com>
2021-09-03 17:34:49 +0000

    centrado

--- Boletas/src/pages/Digitador/Digitador.tsx
+++ Boletas/src/pages/Digitador/Digitador.tsx
@@ -22,36 +22,46 @@ const Digitador: React.FC = () => {
   function Sumar () {
 
   }
-  function Igual () {
+  function Imprimir () {
 
   }
 
     return (
       <IonPage>
           <IonContent>
-            <div className="container">
-              <div className="Calculadora">
-                <div className="display">
-                  <div id="valor-boleta"></div>
+            <div className="container ">
+              <div className="Calculadora ion-text-center">
+                <div className="display ion-padding ion-margin">
+                  <div id="valor-boleta" color="ion-color-new">
+                    
+                  </div>
                 </div>
-                 <IonButton className="col-2" onClick={BorrarTodo}>C</IonButton>
+                <div>
+                 <IonButton className="ion-col-md-2" onClick={BorrarTodo}>Borrar</IonButton>
                  <IonButton className="Flecha" onClick={BorrarUltimo}>D</IonButton>
                  <IonButton className="operador" onClick={Dividir}>%</IonButton>
+                </div>
+                <div>                  
                  <IonButton className="numero">7</IonButton>
                  <IonButton className="numero">8</IonButton>
                  <IonButton className="numero">9</IonButton>
                  <IonButton className="operador" onClick={Multiplicar}>X</IonButton>
+                </div>
+                <div>
                  <IonButton className="numero">4</IonButton>
                  <IonButton className="numero">5</IonButton>
                  <IonButton className="numero">6</IonButton>
                  <IonButton className="operador" onClick={Restar}>-</IonButton>
+                </div>
+                <div>
                  <IonButton className="numero">1</IonButton>
                  <IonButton className="numero">2</IonButton>
                  <IonButton className="numero">3</IonButton>
                  <IonButton className="operador" onClick={Sumar}>+</IonButton>
+                </div>
                  <IonButton className="col-2 numero">0</IonButton>
                  <IonButton className="numero">.</IonButton>
-                 <IonButton className="operador" onClick={Igual}>=</IonButton>
+                 <IonButton className="operador" onClick={Imprimir}>Imprimir</IonButton>
               </div>
             </div>
           </IonContent>
