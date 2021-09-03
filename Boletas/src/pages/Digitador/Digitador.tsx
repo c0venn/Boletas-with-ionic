d34f05f9795d919fb7813348bd4e3dd83aeb25ff
c0venn <fabichidori2@gmail.com>
2021-09-03 19:53:04 +0000

    useState

--- Boletas/src/pages/Digitador/Digitador.tsx
+++ Boletas/src/pages/Digitador/Digitador.tsx
@@ -4,6 +4,12 @@ import { useState } from 'react';
 
 const Digitador: React.FC = () => {
 
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.id));
+  }
+
   function BorrarTodo () {
 
   }
@@ -37,30 +43,30 @@ const Digitador: React.FC = () => {
                   </div>
                 </div>
                 <div>
-                 <IonButton className="ion-col-md-2" onClick={BorrarTodo}>Borrar</IonButton>
-                 <IonButton className="Flecha" onClick={BorrarUltimo}>D</IonButton>
-                 <IonButton className="operador" onClick={Dividir}>%</IonButton>
+                 <IonButton className="ion-col-md-2" id="Borrar" onClick={BorrarTodo}>Borrar</IonButton>
+                 <IonButton className="Flecha" id="Delete-last" onClick={BorrarUltimo}>D</IonButton>
+                 <IonButton className="operador" id="%" onClick={Dividir}>%</IonButton>
                 </div>
                 <div>                  
-                 <IonButton className="numero">7</IonButton>
-                 <IonButton className="numero">8</IonButton>
-                 <IonButton className="numero">9</IonButton>
-                 <IonButton className="operador" onClick={Multiplicar}>X</IonButton>
+                 <IonButton className="numero" id="7" onClick={handleClick}>7</IonButton>
+                 <IonButton className="numero" id ="8" onClick={handleClick}>8</IonButton>
+                 <IonButton className="numero" id ="9" onClick={handleClick}>9</IonButton>
+                 <IonButton className="operador" id ="X" onClick={Multiplicar}>X</IonButton>
                 </div>
                 <div>
-                 <IonButton className="numero">4</IonButton>
-                 <IonButton className="numero">5</IonButton>
-                 <IonButton className="numero">6</IonButton>
-                 <IonButton className="operador" onClick={Restar}>-</IonButton>
+                 <IonButton className="numero" id ="4" onClick={handleClick}>4</IonButton>
+                 <IonButton className="numero" id ="5" onClick={handleClick}>5</IonButton>
+                 <IonButton className="numero" id ="6" onClick={handleClick}>6</IonButton>
+                 <IonButton className="operador" id ="-" onClick={Restar}>-</IonButton>
                 </div>
                 <div>
-                 <IonButton className="numero">1</IonButton>
-                 <IonButton className="numero">2</IonButton>
-                 <IonButton className="numero">3</IonButton>
+                 <IonButton className="numero" id ="1" onClick={handleClick}>1</IonButton>
+                 <IonButton className="numero" id ="2" onClick={handleClick}>2</IonButton>
+                 <IonButton className="numero" id ="3" onClick={handleClick}>3</IonButton>
                  <IonButton className="operador" onClick={Sumar}>+</IonButton>
                 </div>
-                 <IonButton className="col-2 numero">0</IonButton>
-                 <IonButton className="numero">.</IonButton>
+                 <IonButton className="col-2 numero" id ="0"onClick={handleClick}>0</IonButton>
+                 <IonButton className="numero" id ="." onClick={handleClick}>.</IonButton>
                  <IonButton className="operador" onClick={Imprimir}>Imprimir</IonButton>
               </div>
             </div>
